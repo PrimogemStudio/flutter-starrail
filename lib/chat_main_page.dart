6983a65c82f73aa9496e4df7d21d221fa4208075
Jack253-png <guoxiuchen20170402@163.com>
2024-10-03 15:27:37 +0800

    Update

--- lib/chat_main_page.dart
+++ lib/chat_main_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_starrail/packs/starrail_colors.dart';
-import 'package:flutter_starrail/packs/starrail_button.dart';
 import 'package:flutter_starrail/packs/starrail_list.dart';
+import 'package:flutter_starrail/packs/starrail_panel.dart';
 import 'chat_header.dart';
 import 'chat_message_line.dart';
 import 'dart:math';
@@ -10,18 +10,13 @@ import 'dart:math';
 class ChatMainPage extends StatefulWidget {
   ChatMainPage({super.key});
 
-  AnimationController? panelAnimation;
-  Animation? panelTween;
-  Animation<double>? panelRt;
-  Animation<double>? panelOpacity;
-
   @override
   State<ChatMainPage> createState() => ChatMainPageState();
 }
 
 class ChatMainPageState extends State<ChatMainPage> with TickerProviderStateMixin {
   GlobalKey<StarRailListState> key = GlobalKey<StarRailListState>();
-  TextEditingController textCont = TextEditingController();
+  GlobalKey<StarRailPanelState> panelKey = GlobalKey<StarRailPanelState>();
 
   ChatMainPageState() {
     const MethodChannel('MainPage.Event').setMethodCallHandler((call) async {
@@ -78,7 +73,6 @@ class ChatMainPageState extends State<ChatMainPage> with TickerProviderStateMixi
                 width: 50.0, height: 50.0),
             self: false,
             username: "Coder2",
-            // text: '$a,$b',
             text: s,
             msgResv: false,
             onLoadComplete: () {
@@ -86,66 +80,19 @@ class ChatMainPageState extends State<ChatMainPage> with TickerProviderStateMixi
                 key.currentState!.scrollToBottom();
               });
               Future.delayed(Duration(milliseconds: 1500), () {
-                widget.panelAnimation ??= AnimationController(vsync: this, duration: const Duration(milliseconds: 200));
-                if (widget.panelAnimation!.isAnimating) return;
-                widget.panelAnimation!.reset();
-
-                widget.panelRt ??= CurveTween(curve: Curves.fastOutSlowIn).animate(widget.panelAnimation!); // Cubic(.33, .26, 0, .97)
-                widget.panelTween ??= Tween(begin: 0.0, end: 180.0).animate(widget.panelRt!);
-                widget.panelTween!.addListener(() {
-                  panelHeight = widget.panelTween!.value;
-                  setState(() {
-                    key.currentState!.scrollToBottomImm();
-                  });
-                });
-
-                widget.panelOpacity ??= Tween(begin: 0.0, end: 1.0).animate(CurveTween(curve: Curves.easeInBack).animate(widget.panelAnimation!));
-                widget.panelOpacity!.addListener(() {
-                  panelOpacity = max(widget.panelOpacity!.value, 0);
-                });
-
-                widget.panelAnimation!.forward();
+                panelKey.currentState!.openPanel();
               });
             }));
 
-    setState(() {
-      key.currentState!.pushMsg(tt);
-    });
+    setState(() => key.currentState!.pushMsg(tt));
   }
-
-  double panelHeight = 0;
-  double panelOpacity = 0;
 
   @override
   Widget build(BuildContext context) {
     Scaffold sc = Scaffold(
-        body: StarRailList(key: key, innerPanel: Opacity(opacity: panelOpacity, child: Container(height: panelHeight, color: uiPanelBack, child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-          Container(
-            width: 2147483647,
-            height: 4,
-            decoration: BoxDecoration(
-              shape: BoxShape.rectangle,
-              gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    uiPanelBorder,
-                    uiPanelBorderTrans
-                  ]),
-            ),
-          ),
-          TextField(controller: textCont),
-          Padding(padding: EdgeInsets.all(5)),
-          Padding(padding: EdgeInsets.all(10), child: ElevatedButton(
-              onPressed: () {
-                widget.panelAnimation!.animateBack(0);
-                Future.delayed(Duration(milliseconds: 450), () => addMsg(textCont.text));
-              },
-              style: srStyle,
-              child: const Text("Test!")
-          ))
-        ])
-        ))),
+        body: StarRailList(key: key, innerPanel:
+          StarRailPanel(key: panelKey, func: () { addMsg(panelKey.currentState!.getText()); }, onMoving: () => key.currentState!.scrollToBottomImm())
+        ),
         appBar: AppBar(
             backgroundColor: uiSurfaceColor,
             shadowColor: Colors.transparent,
---
+++ lib/packs/starrail_panel.dart
@@ -0,0 +1,90 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_starrail/packs/starrail_button.dart';
+import 'package:flutter_starrail/packs/starrail_colors.dart';
+
+class StarRailPanel extends StatefulWidget {
+  StarRailPanel({
+    super.key,
+    required this.func,
+    required this.onMoving
+  });
+
+  AnimationController? panelAnimation;
+  Animation? panelTween;
+  Animation<double>? panelRt;
+  Animation<double>? panelOpacity;
+  Function func;
+  Function onMoving;
+
+  @override
+  State<StatefulWidget> createState() => StarRailPanelState();
+}
+
+class StarRailPanelState extends State<StarRailPanel> with TickerProviderStateMixin {
+  TextEditingController textCont = TextEditingController();
+  double panelHeight = 0;
+  double panelOpacity = 0;
+
+  void openPanel() {
+    widget.panelAnimation ??= AnimationController(vsync: this, duration: const Duration(milliseconds: 200));
+    if (widget.panelAnimation!.isAnimating) return;
+    widget.panelAnimation!.reset();
+
+    widget.panelRt ??= CurveTween(curve: Curves.fastOutSlowIn).animate(widget.panelAnimation!); // Cubic(.33, .26, 0, .97)
+    widget.panelTween ??= Tween(begin: 0.0, end: 180.0).animate(widget.panelRt!);
+    widget.panelTween!.addListener(() {
+      panelHeight = widget.panelTween!.value;
+      setState(() {
+        widget.onMoving();
+      });
+    });
+
+    widget.panelOpacity ??= Tween(begin: 0.0, end: 1.0).animate(CurveTween(curve: Curves.easeInBack).animate(widget.panelAnimation!));
+    widget.panelOpacity!.addListener(() {
+      panelOpacity = max(widget.panelOpacity!.value, 0);
+    });
+
+    widget.panelAnimation!.forward();
+  }
+
+  void closePanel() {
+    widget.panelAnimation!.animateBack(0);
+  }
+
+  String getText() {
+    return textCont.text;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var i = Container(height: panelHeight, color: uiPanelBack, child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+      Container(
+        width: 2147483647,
+        height: 4,
+        decoration: BoxDecoration(
+          shape: BoxShape.rectangle,
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                uiPanelBorder,
+                uiPanelBorderTrans
+              ]),
+        ),
+      ),
+      TextField(controller: textCont),
+      Padding(padding: EdgeInsets.all(5)),
+      Padding(padding: EdgeInsets.all(10), child: ElevatedButton(
+          onPressed: () {
+            closePanel();
+            Future.delayed(Duration(milliseconds: 450), () => widget.func());
+          },
+          style: srStyle,
+          child: const Text("Test!")
+      ))
+    ]));
+    return Opacity(opacity: panelOpacity, child: i);
+  }
+}
