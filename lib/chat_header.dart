c84b2886ae9963aeb565e53970dad9cc0ad9f195
hackermdch <58121864+hackermdch@users.noreply.github.com>
2024-10-05 08:30:19 +0800

    Update

--- lib/chat_header.dart
+++ lib/chat_header.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'headers/header_chatting.dart';
 
-const replyer = "hackerm大神";
-const replyerDesc = "一个一般路过的普通开发者~";
-
 class ChatHeader extends StatefulWidget {
   ChatHeader({super.key, required this.replyer, required this.replyerDesc});
 
@@ -34,15 +31,21 @@ class ChatHeaderState extends State<ChatHeader> with TickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-    widget.mainAnimation = AnimationController(vsync: this, duration: const Duration(milliseconds: 300));
-    widget.opacityAnimation = CurveTween(curve: Curves.easeOutExpo).animate(widget.mainAnimation!);
-    widget.offsetAnimation = Tween<Offset>(
-        begin: const Offset(0.15, 0), end: const Offset(0, 0))
-        .animate(CurvedAnimation(
-        parent: widget.mainAnimation!,
-        curve: Curves.easeOutExpo,
-        reverseCurve: StaticCurve()));
+    widget.mainAnimation = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 300));
+    widget.opacityAnimation =
+        CurveTween(curve: Curves.easeOutExpo).animate(widget.mainAnimation!);
+    widget.offsetAnimation =
+        Tween<Offset>(begin: const Offset(0.15, 0), end: const Offset(0, 0))
+            .animate(CurvedAnimation(
+                parent: widget.mainAnimation!,
+                curve: Curves.easeOutExpo,
+                reverseCurve: StaticCurve()));
     widget.mainAnimation!.forward();
+    updateText(() {
+      widget.replyer = "聊天频道";
+      widget.withDesc = false;
+    });
   }
 
   @override
@@ -65,10 +68,17 @@ class ChatHeaderState extends State<ChatHeader> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return SlideTransition(position: widget.offsetAnimation!, child: FadeTransition(opacity: widget.opacityAnimation!, child: Card(
-        color: Colors.transparent,
-        shadowColor: Colors.transparent,
-        margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
-        child: HeaderChatting(replyer: widget.replyer, replyerDesc: widget.replyerDesc, withDesc: widget.withDesc))));
+    return SlideTransition(
+        position: widget.offsetAnimation!,
+        child: FadeTransition(
+            opacity: widget.opacityAnimation!,
+            child: Card(
+                color: Colors.transparent,
+                shadowColor: Colors.transparent,
+                margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                child: HeaderChatting(
+                    replyer: widget.replyer,
+                    replyerDesc: widget.replyerDesc,
+                    withDesc: widget.withDesc))));
   }
 }
